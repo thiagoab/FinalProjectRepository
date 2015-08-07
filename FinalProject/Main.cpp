ce758535f9208cafd4c851a06ff6bd19f01edd02
thiagoab <thiago.alves.braga@gmail.com>
2015-08-07 08:39:18 -0700

    Including initial project in repository

---
+++ FinalProject/Main.cpp
@@ -0,0 +1,17 @@
+#include "include\GL\glew.h"
+#include "include\GL\freeglut.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 500);//optional
+	glutInitWindowSize(800, 600); //optional
+	glutCreateWindow("OpenGL First Window");
+
+	glutMainLoop();
+
+	return 0;
+}
